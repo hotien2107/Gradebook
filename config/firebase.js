30fcf91ecf0298fdac2a320f9f20ba046eb31b33
Ho Tien <hotien2107@gmail.com>
2022-01-01 09:55:43 +0700

    add env

--- config/firebase.js
+++ config/firebase.js
@@ -3,14 +3,14 @@ import { getFirestore } from 'firebase/firestore';
 import { getAuth } from 'firebase/auth';
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyD9lHtR8MAGgf-W0EBTQh8S5xY9dyuv-AE',
-  authDomain: 'goodjobs-fcadd.firebaseapp.com',
-  databaseURL: 'https://goodjobs-fcadd-default-rtdb.asia-southeast1.firebasedatabase.app',
-  projectId: 'goodjobs-fcadd',
-  storageBucket: 'goodjobs-fcadd.appspot.com',
-  messagingSenderId: '930053222925',
-  appId: '1:930053222925:web:55acd629ec7fa90a7e5b44',
-  measurementId: 'G-JB7MGTPB95',
+  apiKey: process.env.NEXT_PUBLIC_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_AUTH_DOMAIN,
+  databaseURL: process.env.NEXT_PUBLIC_DATABASE_URL,
+  projectId: process.env.NEXT_PUBLIC_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
